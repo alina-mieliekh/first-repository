3a4f11c4cfcc6302759b1b70ec2b0829fdbbac50
Alina Mieliekh <alina.mieliekh@gmail.com>
2014-10-14 22:17:51 +0200

    for added.

--- TestClass.java
+++ TestClass.java
@@ -4,9 +4,13 @@ public class TestClass {
 	public static void main(String[] args) {
 	
 		String word = "Hello World!";
+		
+		for (int i=0; i<=5; i++){
 			
-		System.out.println(word);
-
+			System.out.println(word + " " + i);
+			
+		}
+			
 	}
 
 }
